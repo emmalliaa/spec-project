162435ee4e3e2f53fbdc3813e80df03768b543f9
emmalliaa <emmaliss001@gmail.com>
2022-11-10 12:34:05 +0100

    new idea for the project
    coding the index file and trying to make the images animate with js using the gsap toolset

--- java/main.js
+++ java/main.js
@@ -1,4 +1,7 @@
-//jQueries for my SVG to load 
-$(document).ready (function(){
-    $("#mySVG").load("image/seats.svg")
-});
+TweenMax.staggerFrom(".container > .block", 2,{
+    y:"100%",
+
+    delay: 0.1
+
+},
+0.8)
