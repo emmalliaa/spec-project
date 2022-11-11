224b105ed5c039cd6f800a7e43010777b3880392
emmalliaa <emmaliss001@gmail.com>
2022-11-11 14:55:34 +0100

    added more animation
    made few changes in css to make the block with images be in the center and added new js for the navigation bar to appear with delay and also the container to  change its size for the title to be visible

--- java/main.js
+++ java/main.js
@@ -1,7 +1,36 @@
-TweenMax.staggerFrom(".container > .block", 2,{
-    y:"100%",
-
-    delay: 0.1
+TweenMax.staggerFrom(".container > .block", 1,{
+    y:"110%",
+    delay: 0.1,
+    ease:Expo.easeInOut
 
 },
-0.8)
+0.5);
+
+TweenMax.to(".container",2,{
+    scale:"1",
+    y:"90%",
+    ease:Expo.easeInOut,
+    delay:1.2,
+});
+
+TweenMax.to(".container",2,{
+    scale:"2",
+    y:"90%",
+    ease:Expo.easeInOut,
+    delay:1.2,
+});
+
+TweenMax.staggerFrom(".navbar>.logo-name",1,{
+    opacity:0,
+    y:-100,
+    ease:Expo.easeInOut,
+    delay:1,
+},0.1);
+
+TweenMax.staggerFrom(".navbar>.menu>.menu-item",1,{
+    opacity:0,
+    y:-100,
+    ease:Power2.easeOut,
+    delay:1 ,
+
+},0.1);
