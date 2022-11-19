29e0f5b7c5bc2552829364e87beeea007d1c35e8
emmalliaa <emmaliss001@gmail.com>
2022-11-19 16:53:33 +0100

    crating timelines in js
    putting animations in nested timelines to make the code more sustainable and easier to adjust

--- java/main.js
+++ java/main.js
@@ -1,44 +1,42 @@
-
-gsap.to(".container",{duration:2,
-    y:"90%",
-    ease:"Expo.in",
-    delay:2.8,
-});
-
-
-gsap.from(".logo-name",{
-    opacity:0,
-    y:-100,
-    ease:"Expo.in",
+//navbar animation
+gsap.from(".logo-name",
+{opacity:0,
     delay:0,
-
-    
+    y:"-100%",
+    ease:"Expo.in"
 
 });
 
-gsap.from(".menu-item",{duration:1.1,
+gsap.from(".menu-item",{
     opacity:0,
-    y:-100,
-    ease:"Power2.easeOut",
-    delay:2.7 ,
+    delay:1.8,
+    y:"-100%",
 
-});
+})
 
-gsap.to(".container",{duration:2,
-    y:"0%",
-    ease:"expo",
-    delay:5.8,
-});
+//animations in nested timeline 
+function firstLoad() {
+    const tl= gsap.timeline();
+    tl.from(".block", {y:900,ease:"Expo.in",opacity:0, stagger:{each:0.5,}});
+    tl.to(".container", {y:"80%",scale:"1.3",ease:"power4"});
+    tl.from(".title",{ opacity:0
+    } )
+    tl.to(".container",{y:"0%",scale:1,delay:"0.6",ease:"Expo.in"})
+    return tl;
 
-function first() {
-    var tl= gsap.timeline();
-    tl.from(".block", {y:900,ease:"Expo.in",stagger:{each:0.9,}});
-    tl.to(".container",)
-
+}
+function carousel() {
+    const tl=gsap.timeline();
+    tl.to(".b-3", {duration:0.2,x:"80%",scale:0.7, ease:"expo.inOut"});
+    tl.to(".b-2", {duration:0.2,x:"-80%",scale:0.7,ease:"expo.inOut"});
+    tl.to(".b-1", {scale:0.7,ease:"expo.inOut"})
+    tl.to(".container", {delay:"0.8",y:"50%",ease:"circ"})
     return tl;
 }
-var final= gsap.timeline();
-final.add(first())
+
+const final= gsap.timeline();
+final.add(firstLoad())
+final.add(carousel())
 
 
 
